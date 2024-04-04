3140a83f1ce05b1a08f7ec09b57f521cf3509ad4
Amdiatou TIMERA <amdiatoutimera@hotmail.fr>
2024-04-04 20:28:41 +0200

    Premier commit: Création du projetÃ

---
+++ src/test/java/com/amdiatou/atiservices/AtiservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdiatou.atiservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
