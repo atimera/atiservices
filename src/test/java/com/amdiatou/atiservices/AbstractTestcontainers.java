f504a4faecf8351d79d9dca24bfeb58c6bf31d1f
Amdiatou TIMERA <amdiatoutimera@hotmail.fr>
2024-04-15 00:03:17 +0200

    Test unitaires: VilleJDBCDataAccessService

--- src/test/java/com/amdiatou/atiservices/AbstractTestcontainers.java
+++ src/test/java/com/amdiatou/atiservices/AbstractTestcontainers.java
@@ -2,11 +2,15 @@ package com.amdiatou.atiservices;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
 
 @Testcontainers
 public abstract class AbstractTestcontainers {
@@ -31,7 +35,7 @@ public abstract class AbstractTestcontainers {
                     .withPassword("password");
 
     @DynamicPropertySource
-    private static void registerDatasourceProperties(DynamicPropertyRegistry registry){
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry){
         registry.add(
                 "spring.datasource.url",
                 postgreSQLContainer::getJdbcUrl
@@ -44,7 +48,19 @@ public abstract class AbstractTestcontainers {
                 "spring.datasource.password",
                 postgreSQLContainer::getPassword
         );
+    }
 
+    private static DataSource getDataSource(){
+        return DataSourceBuilder.create()
+                .driverClassName(postgreSQLContainer.getDriverClassName())
+                .url(postgreSQLContainer.getJdbcUrl())
+                .username(postgreSQLContainer.getUsername())
+                .password(postgreSQLContainer.getPassword())
+                .build();
+    }
+
+    protected static JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
     }
 
 }
